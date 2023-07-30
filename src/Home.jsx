7814deff3cb62d80ba50b8875d5d2dd0e8ec3412
unknown <shalinisai9195@gmail.com>
2023-07-30 08:58:49 +0530

    update commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 
 function Home(){
 
     return (
-        <>
-        welcome to  Home Page..
+        <div className='bg-light vh-100'>
+        <div className='d-md-flex justify-content-md-center pt-4 gap-4 ' >
+            <h3  className='fst-italic fw-semibold'>Welcome to  Home Page! </h3> 
+              <div className='d-grid gap-3 d-md-flex'>
+                <Link to='/register' type="button" className="btn btn-outline-info">Register</Link>
         
-        </>
+                <Link to='/login' type="button" className="btn btn-outline-secondary">Login</Link>
+               </div>
+       
+        
+        </div>
+        </div>
     )
 
 }
